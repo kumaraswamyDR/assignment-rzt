c6f4736b61d0e2aab66225a0b274f671a1397650
kumaraHB <kumara@honeybridge.com>
2020-07-31 14:03:45 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from "react-router-dom";
+import {Provider} from "react-redux";
+import {createStore,applyMiddleware} from "redux";
+import createSagaMiddleware from "redux-saga"
+import rootReducer from './reducers/index'
+import rootSaga from './sagas'
+import { createBrowserHistory } from "history"
+
+
+const sagaMiddleware = createSagaMiddleware();
+const history = createBrowserHistory();
+const store = createStore(rootReducer,applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Router history={history} >
+            <Provider store={store}>
+            <App />
+            </Provider>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
